1e9d6515c7f7959fced3defc2261896bd0ecfb63
Anna <annapn5@mail.ru>
2022-12-24 18:26:00 +0600

    Создала ответ на задачу 3 второго урока

---
+++ program3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+void NumOfWeek()
+{
+    Console.WriteLine("Введите целое число:");
+    string nu = Console.ReadLine();
+    int number = Convert.ToInt32(nu);
+    if(number>0 && number<6)
+    {
+        Console.WriteLine("День недели является будним днём");
+    }
+    else if(number>5 && number<8)
+    {
+        Console.WriteLine("День недели является выходным днём.");
+    }
+    else
+    {
+        Console.WriteLine("Данное число нельзя отнести ко дню недели");
+    }
+}
+NumOfWeek();
