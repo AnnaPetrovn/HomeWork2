b383c6f01de0177be15f57632d7b9630c9b9a3de
Anna <annapn5@mail.ru>
2022-12-24 12:00:22 +0600

    Сделала задание 1 ко второму уроку

---
+++ program1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+void SecondOfThree()
+{
+    Console.WriteLine("Введите целое трехзначное число:");
+    string nu = Console.ReadLine();
+    int number = Convert.ToInt32(nu);
+    number = number/10;
+    number = number%10;
+    Console.WriteLine("Вторая цифра трехзначеного числа " + number);
+}
+SecondOfThree();
